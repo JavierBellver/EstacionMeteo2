8d74c2da3b18afa734ff7f14d4523e952a5c8f76
Carlos Rey Planelles <carlosreyplanelles@gmail.com>
2015-12-03 16:11:18 +0100

    no message

---
+++ InterfazEstacion/InterfazEstacion/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace InterfazEstacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Consultar_Click(object sender, EventArgs e)
+        {
+            int res=0;
+            Boolean resb = true;
+            EstMeteo.EstacionMeteo em = new EstMeteo.EstacionMeteo();
+            switch (comboBox_Atributo.Text)
+            {
+                case "Temperatura":
+                    em.getTemperatura(out res, out resb);
+                    label_resultados.Text = "Temperatura=";
+                    break;
+                case "Humedad":
+                    em.getHumedad(out res, out resb);
+                    label_resultados.Text = "Humedad=";
+                    break;
+                case "Luminosidad":
+                    em.getLuminosidad(out res, out resb);
+                    label_resultados.Text = "Luminosidad=";
+                    break;
+            }
+
+
+            label_resultados.Text = label_resultados.Text + res;
+
+
+        }
+
+        private void Button_Conectar_Click(object sender, EventArgs e)
+        {
+            listView_estacionesReg.Items.Add(textBox_direccion.Text);
+            comboBox_Estacion.Items.Insert(comboBox_Estacion.Items.Count, textBox_direccion.Text);
+
+        }
+    }
+}
